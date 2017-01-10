c94163def1c8991adf5ccfe10c66727238f9303b
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-10 18:22:45 +0000

    basic styling

--- client/src/components/Top20Entry.jsx
+++ client/src/components/Top20Entry.jsx
@@ -3,7 +3,12 @@ var React = require('react');
 var Top20Entry = React.createClass({
 
   render: function() {
-    return <img src={ this.props.entryData["im:image"][2].label } />
+    return (
+      <img
+        className="Top20Image"
+        src={ this.props.entryData["im:image"][2].label }
+      />
+    )
   }
 
 });
